161d98268c10e1dd760c2ea4a7b40399459e5872
Yash Raj Sharma <110107845+haishesauke@users.noreply.github.com>
2024-07-09 16:29:28 +0000

    added some functions

--- ocr.js
+++ ocr.js
@@ -1,8 +1,10 @@
 var ocrDemo = {
-    CANVAS_WIDTH : 200,
-    TRANSLATED_WIDTH : 20,
-    PIXEL_WIDTH : 10,
+    CANVAS_WIDTH: 200,
+    TRANSLATED_WIDTH: 20,
+    PIXEL_WIDTH: 10,
     BLUE: 'blue', // Assuming BLUE is a constant color string
+    data: [], // Assuming data is initialized somewhere else
+
     drawGrid: function(ctx) {
         for (var x = this.PIXEL_WIDTH, y = this.PIXEL_WIDTH;
             x < this.CANVAS_WIDTH;
@@ -22,5 +24,31 @@ var ocrDemo = {
             ctx.lineTo(this.CANVAS_WIDTH, y);
             ctx.stroke();
         }
+    },
+
+    onMouseMove: function(e, ctx, canvas) {
+        if (!canvas.isDrawing) {
+            return;
+        }
+        this.fillSquare(ctx, e.clientX - canvas.offsetLeft, e.clientY - canvas.offsetTop);
+    },
+
+    onMouseDown: function(e, ctx, canvas) {
+        canvas.isDrawing = true;
+        this.fillSquare(ctx, e.clientX - canvas.offsetLeft, e.clientY - canvas.offsetTop);
+    },
+
+    onMouseUp: function(e, canvas) {
+        canvas.isDrawing = false;
+    },
+
+    fillSquare: function(ctx, x, y) {
+        var xPixel = Math.floor(x / this.PIXEL_WIDTH);
+        var yPixel = Math.floor(y / this.PIXEL_WIDTH);
+        this.data[((xPixel - 1) * this.TRANSLATED_WIDTH + yPixel) - 1] = 1;
+        ctx.fillStyle = '#ffffff';
+        ctx.fillRect(xPixel * this.PIXEL_WIDTH, yPixel * this.PIXEL_WIDTH,
+            this.PIXEL_WIDTH, this.PIXEL_WIDTH
+        );
     }
 };
